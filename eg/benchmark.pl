b34aabecfb642c22c8076e0927d2cc998b3d5ebe
Masahiro Nagano <kazeburo@gmail.com>
2014-10-26 01:57:02 +0900

    benchmark

--- eg/benchmark.pl
+++ eg/benchmark.pl
@@ -19,22 +19,16 @@ say $fast->get('foo');
 
 
 cmpthese(
-    -1,
+    20_000,
     {
         fast => sub {
-            for (1..10) {
-                my $val = $fast->get('foo');
-            }
+            my $val = $fast->get('foo');
         },
         tiny => sub {
-            for (1..10) {
-                my $data = $tiny->command(qw/get foo/);
-            }
+            my $data = $tiny->command(qw/get foo/);
         },
         redis => sub {
-            for (1..10) {
-                my $data = $redis->get('foo');
-            }
+            my $data = $redis->get('foo');
         },
     }
 );
@@ -42,27 +36,19 @@ cmpthese(
 print "single incr =======\n";
 
 cmpthese(
-    -1,
+    20_000,
     {
         fast => sub {
-            for (1..10) {
-                my $val = $fast->incr('incrfoo');
-            }
+            my $val = $fast->incr('incrfoo');
         },
         tiny => sub {
-            for (1..10) {
-                my $data = $tiny->command(qw/incr incrfoo/);
-            }
+            my $data = $tiny->command(qw/incr incrfoo/);
         },
         tiny_noreply => sub {
-            for (1..10) {
-                $tiny_noreply->command(qw/incr incrfoo/);
-            }
+            $tiny_noreply->command(qw/incr incrfoo/);
         },
         redis => sub {
-            for (1..10) {
-                my $data = $redis->incr('incrfoo');
-            }
+            my $data = $redis->incr('incrfoo');
         },
     }
 );
@@ -71,45 +57,37 @@ print "pipeline =======\n";
 
 my $cb = sub {};
 cmpthese(
-    -1,
+    20_000,
     {
         fast => sub {
-            for (1..10) {
-                $fast->del('user-fail',$cb);
-                $fast->del('ip-fail',$cb);
-                $fast->lpush('user-log','xxxxxxxxxxx',$cb);
-                $fast->lpush('login-log','yyyyyyyyyyy',$cb);
-                $fast->wait_all_responses;
-            }
+            $fast->del('user-fail',$cb);
+            $fast->del('ip-fail',$cb);
+            $fast->lpush('user-log','xxxxxxxxxxx',$cb);
+            $fast->lpush('login-log','yyyyyyyyyyy',$cb);
+            $fast->wait_all_responses;
         },
         tiny => sub {
-            for (1..10) {
-                my $val = $tiny->command(
-                    [qw/del user-fail/],
-                    [qw/del ip-fail/],
-                    [qw/lpush user-log xxxxxxxxxxx/],
-                    [qw/lpush login-log yyyyyyyyyyy/]
-                );
-            }
+            my $val = $tiny->command(
+                [qw/del user-fail/],
+                [qw/del ip-fail/],
+                [qw/lpush user-log xxxxxxxxxxx/],
+                [qw/lpush login-log yyyyyyyyyyy/]
+            );
         },
         tiny_noreply => sub {
-            for (1..10) {
-                $tiny_noreply->command(
-                    [qw/del user-fail/],
-                    [qw/del ip-fail/],
-                    [qw/lpush user-log xxxxxxxxxxx/],
-                    [qw/lpush login-log yyyyyyyyyyy/]
-                );
-            }
+            $tiny_noreply->command(
+                [qw/del user-fail/],
+                [qw/del ip-fail/],
+                [qw/lpush user-log xxxxxxxxxxx/],
+                [qw/lpush login-log yyyyyyyyyyy/]
+            );
         },
         redis => sub {
-            for (1..10) {
-                $redis->del('user-fail',$cb);
-                $redis->del('ip-fail',$cb);
-                $redis->lpush('user-log','xxxxxxxxxxx',$cb);
-                $redis->lpush('login-log','yyyyyyyyyyy',$cb);
-                $redis->wait_all_responses;
-            }
+            $redis->del('user-fail',$cb);
+            $redis->del('ip-fail',$cb);
+            $redis->lpush('user-log','xxxxxxxxxxx',$cb);
+            $redis->lpush('login-log','yyyyyyyyyyy',$cb);
+            $redis->wait_all_responses;
         },
     }
 );
